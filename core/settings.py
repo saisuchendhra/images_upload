0b367cca65c07010f7005055c9deebaba3911d15
saisuchendhra <saisuchendhra@gmail.com>
2023-03-02 17:08:32 -0700

    'asads'

--- core/settings.py
+++ core/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'images_db',
-    #     'USER': 'sai',
-    #     'PASSWORD': 'sai',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'images_db',
+        'USER': 'sai',
+        'PASSWORD': 'sai',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
